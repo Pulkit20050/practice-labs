1d017c56bc8c9ace9a81ab11d04a0e2221019d9a
Pulkit Raj <148369493+Pulkit20050@users.noreply.github.com>
2026-05-25 08:11:38 +0530

    Create booth.sv

---
+++ multipliers/booth.sv
@@ -0,0 +1,67 @@
+module booth#(parameter b=4)
+  (
+    input logic [b-1:0] m,
+    input logic [b-1:0] n,
+    input logic enable,
+    output logic [b-1:0] product);
+  logic [b-1:0] Q;
+  logic [b-1:0] A;
+  logic Q_1;
+  logic [b-1:0];
+  //states declaration
+  parameter init=2'b00;
+  parameter check=2'b01;
+  parameter shift=2'b10;
+  parameter idle=2'b11;
+  logic state[1:0];
+  always@(state) begin
+    case(state) begin
+      idle: begin
+        A<=0;
+        Q<=0;
+        Q_1<=0;
+        product<=0; end
+      init: begin
+        A<=0; 
+        Q<=n;
+        Q_1<=0;
+        state<=check;
+      end
+      check: begin
+        case({Q[0],Q_1}) begin
+          10: begin
+            A<=A-m;
+            state<=shift;
+          end
+          01: begin
+            A<=A+m;
+            state<=shift;
+          end
+          default: state<=shift;
+          endcase
+        end
+        shift: begin
+          if(count==b-1) begin
+            state<=init;
+            product<={A,Q};
+          end
+          else begin
+            {A,Q,Q_1}<={A[b-1], A, Q};
+            count<=count+1;
+          end
+        end
+        endcase
+      end
+    end
+      always@(reset, enable)
+        if(reset) begin
+          state<=idle;
+        end
+        else if(enable) begin
+          state<=init; end
+endmodule
+          
+        
+
+        
+  
